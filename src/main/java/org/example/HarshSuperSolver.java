ff3185dd1413e0d9db89ae6c41ff79e9264da4ad
patelhar <harshyboi2004@gmail.com>
2023-01-30 19:25:53 -0600

    Some code from yours truly, with some enhancements to the example

--- src/main/java/org/example/HarshSuperSolver.java
+++ src/main/java/org/example/HarshSuperSolver.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExamplePathFinder {
+public class HarshSuperSolver {
 
     private Vec2d startPos;
     private Vec2d endPos;
@@ -12,7 +12,7 @@ public class ExamplePathFinder {
 
     private List<Vec2d> frontierNodes;
 
-    public ExamplePathFinder(Vec2d startPos, Vec2d endPos, boolean[][] map) {
+    public HarshSuperSolver(Vec2d startPos, Vec2d endPos, boolean[][] map) {
         this.startPos = startPos;
         this.endPos = endPos;
         this.map = map;
@@ -40,7 +40,7 @@ public class ExamplePathFinder {
         // set the foundPath to true
 
 
-        boolean foundPath = true; //set to true when path is found
+        boolean foundPath = false; //set to true when path is found
 
 
 
@@ -52,10 +52,19 @@ public class ExamplePathFinder {
             //a value of 10 means you have to move 10 times...
             //initally the cost map will be all 0's, you need to fill it up in here
 
-            for (Vec2d point : frontierNodes) {
-                //go through every point on the frontier node
+            var nextFrontier = new ArrayList<Vec2d>();
 
+            while (frontierNodes.size() > 0) {
+                var point = frontierNodes.get(0);
+
+                var temp = findPathAroundPoint(point);
+                for (Vec2d i : temp) {
+                    nextFrontier.add(i);
+                }
+
+                frontierNodes.remove(0);
             }
+
 
             foundPath = false;
             //if you find the path, set this to true
@@ -64,7 +73,26 @@ public class ExamplePathFinder {
 
     }
 
+    public List<Vec2d> findPathAroundPoint(Vec2d point) {
+        var returner = new ArrayList<Vec2d>();
+
+        if (point.x + 1 < costMap.length && costMap[point.x + 1][point.y] == 0) {
+            returner.add(new Vec2d(point.x + 1, point.y));
+        }
+        if (point.x - 1 > 0 && costMap[point.x - 1][point.y] == 0) {
+            returner.add(new Vec2d(point.x - 1, point.y));
+        }
+        if (point.y + 1 < costMap[0].length && costMap[point.x][point.y + 1] == 0) {
+            returner.add(new Vec2d(point.x, point.y + 1));
+        }
+        if (point.y - 1 > 0 && costMap[point.x][point.y - 1] == 0) {
+            returner.add(new Vec2d(point.x, point.y - 1));
+        }
+
+        return returner;
+    }
     public List<Vec2d> findAllFrontierPoints() {
+        var returner = new ArrayList<Vec2d>();
 
         //TODO implement
         //make this class return all the points that we are exploring
@@ -94,8 +122,6 @@ public class ExamplePathFinder {
 
     public void printSolution() {
 
-        System.out.println("Example command's code");
-
         for (int x = 0; x < costMap.length; x++) {
             for (int y = 0; y < costMap[0].length; y++) {
 
@@ -107,10 +133,6 @@ public class ExamplePathFinder {
                     character = "S ";
                 } else if (x == endPos.x && y == endPos.y) {
                     character = "E ";
-                }
-
-                if (character.equals("0")) {
-                    character = "_ ";
                 }
 
                 System.out.print(character);
